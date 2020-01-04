e87ff6a4716ff5fcbb3f5b4090d384aa7ee09432
judith <judith.klix@uni-weimar.de>
2020-01-04 12:29:01 +0100

    add LightNode

---
+++ framework/include/PointLightNode.hpp
@@ -0,0 +1,21 @@
+#ifndef POINTLIGHTNODE_HPP
+#define POINTLIGHTNODE_HPP
+
+#include<glm/glm.hpp>
+#include"Node.hpp"
+#include"model.hpp"
+
+class PointLightNode: public Node
+{
+    public:
+    PointLightNode();
+    PointLightNode(float lightIntensity, glm::vec3  lightColor);
+
+    private:
+    float lightIntensity_;
+    glm::vec3 lightColor_;
+
+};
+
+
+#endif //PointLightNode_HPP
